be760569aeec8efa04327f1d41e9c3e65fd0aa6c
Thierno-Diallo <thiernosouley@gmail.com>
2021-10-25 16:36:08 +0200

    final commit @AFPA 25/10

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-window.onload = ( ) => {
+window.onload = () => {
     const elts = document.querySelectorAll(".accordion-element");
 
     const firstActive = document.querySelector(".accordion .active");
 
-    let section = firstActive.children[1].querySelector("P");
+    
+    let section = firstActive.children[1].
+    querySelector("p");
+            //réccup hauteur de la section
+    let sectionHeight = section.offsetHeight + 20;
+    // déf hauteur
+    firstActive.children[1].style.height  =
+    sectionHeight+"px";
 
-    let sectionHeight =section.offsetheight + 20;
+    // ouverture/fermeture d'element
 
-    firstActive.children[1].style.height = sectionHeight +"px";
+    for(let elt of elts){
+        elt.addEventListener("click", function(){
+            //réccup element actif
+        const active = document.querySelector(".accordion .active");
+        const sectionHeight = this.offsetHeight + 50;
+
+            // on rétire la class active
+            if(this.classList.contains('active')){
+                active.classList.remove("active");
+                active.children[1].style.height = 0;
+            }else{
+                this.classList.add('active');
+                this.children[1].style.height =
+                sectionHeight+"px";
+            }
+            console.log(this);
+        })
+    }
+    
+
+
+
+}
